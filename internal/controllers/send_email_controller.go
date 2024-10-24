e8366ac79b16b8c733f337ed0d4753941745e0cc
Fabricio Cosati <fabricio18cosati@gmail.com>
2024-10-23 23:46:58 -0300

    creating an email template to portfolio

--- internal/controllers/send_email_controller.go
+++ internal/controllers/send_email_controller.go
@@ -15,7 +15,16 @@ func SendEmailHandler(c *fiber.Ctx) error {
 	}
 
 	d := services.GetMailDialer(*email)
-	message := services.GetMailMessage(*email)
+
+	body, err := services.LoadEmailBody(c, *email)
+
+	if err != nil {
+		log.Printf("Error on load email template: %v", err)
+		c.Status(500).SendString("Internal Server Error")
+		panic(err)
+	}
+
+	message := services.GetMailMessage(*email, body)
 
 	if err := d.DialAndSend(message); err != nil {
 		log.Printf("Error sending email: %v", err)
--- internal/services/send_email_services.go
+++ internal/services/send_email_services.go
@@ -1,20 +1,23 @@
 package services
 
 import (
-	"fmt"
+	"bytes"
+	"io"
 
 	"github.com/fabricio-cosati/portfolio/cmd/dto"
+	"github.com/fabricio-cosati/portfolio/internal/templates/pages"
+	"github.com/gofiber/fiber/v2"
 	gomail "gopkg.in/mail.v2"
 )
 
-func GetMailMessage(email dto.Email) *gomail.Message {
+func GetMailMessage(email dto.Email, body string) *gomail.Message {
 	message := gomail.NewMessage()
 
 	message.SetHeader("From", email.Email)
 	message.SetHeader("To", "fabricio18cosati@gmail.com")
 	message.SetHeader("Subject", "Portfolio Contact")
 	message.SetHeader("Reply-To", email.Email)
-	message.SetBody("text/plain", fmt.Sprintf("Você recebeu um email de %v \n\n%v", email.Name, email.Message))
+	message.SetBody("text/html", body)
 
 	return message
 }
@@ -28,4 +31,18 @@ func GetMailDialer(email dto.Email) *gomail.Dialer {
 		smtpCredentials.Username,
 		smtpCredentials.Password,
 	)
+}
+
+func LoadEmailBody(c *fiber.Ctx, email dto.Email) (string, error) {
+	var buf bytes.Buffer
+
+	bodyTemplate := pages.Email("Email", email)
+	writer := io.Writer(&buf)
+	err := bodyTemplate.Render(c.Context(), writer)
+
+	if err != nil {
+		return "", err
+	}
+
+	return buf.String(), nil
 }
